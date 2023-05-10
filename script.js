146822dd1b30009862ed25b63ba475a4f011f61e
Levi Martins <99222268+Levi-Martins@users.noreply.github.com>
2023-05-09 23:21:54 -0300

    teste01

---
+++ script.js
@@ -0,0 +1,38 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+const cardImg = document.querySelectorAll(".card-img");
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    const isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+    cardImg.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      inline: "center",
+      behavior: "smooth",
+      block:"nearest"
+    });
+
+    items[currentItem].classList.add("current-item");
+    cardImg[currentItem].classList.add("current-item");
+    console.log(currentItem)
+  });
+});
